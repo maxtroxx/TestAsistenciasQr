989126333843604fda3acaeb38065b703c0abb6e
luis valenzuela <101830105+maxtroxx@users.noreply.github.com>
2024-03-14 10:57:20 -0300

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,59 +1,48 @@
+// Función para iniciar el escaneo del código QR
 function iniciarEscaneo() {
-    var scanner = new Instascan.Scanner({ video: document.createElement('canvas') }); // Utiliza un elemento de Canvas en lugar de video
+    var scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+    var hoja = document.getElementById('hoja').value;
     scanner.addListener('scan', function (contenidoQR) {
         // Una vez que se escanea el código QR, se envían los datos a Google Sheets
-        enviarDatosAGoogleSheets(contenidoQR);
+        enviarDatosAGoogleSheets(contenidoQR, hoja);
         scanner.stop(); // Detener el escaneo después de un código QR exitoso
     });
-
-    navigator.mediaDevices.enumerateDevices()
-        .then(devices => {
-            var cameras = devices.filter(device => device.kind === 'videoinput');
-            var selectedCamera = cameras.length > 1 ? cameras[1].deviceId : cameras[0].deviceId;
-
-            navigator.mediaDevices.getUserMedia({
-                    video: {
-                        deviceId: selectedCamera ? { exact: selectedCamera } : undefined,
-                        facingMode: 'environment' // Intenta usar la cámara trasera si está disponible
-                    }
-                })
-                .then(stream => {
-                    scanner.start(stream);
-                })
-                .catch(error => {
-                    console.error('Error al acceder a la cámara:', error);
-                    alert('Error al acceder a la cámara: ' + error);
-                });
-        })
-        .catch(error => {
-            console.error('Error al enumerar los dispositivos de medios:', error);
-            alert('Error al enumerar los dispositivos de medios: ' + error);
-        });
+    Instascan.Camera.getCameras().then(function (cameras) {
+        if (cameras.length > 0) {
+            // Buscar la cámara trasera y usarla si está disponible
+            var camera = cameras.find(function (camera) {
+                return camera.name.toLowerCase().includes('back') || camera.name.toLowerCase().includes('rear');
+            });
+            scanner.start(camera || cameras[0]); // Iniciar el escaneo con la cámara seleccionada
+        } else {
+            alert('No se detectó ninguna cámara en el dispositivo.');
+        }
+    }).catch(function (e) {
+        console.error(e);
+        alert('Error al acceder a la cámara: ' + e);
+    });
 }
 
-
-// Función para enviar los datos a Google Sheets
-function enviarDatosAGoogleSheets(contenidoQR) {
-    console.log(contenidoQR);
-    var url = 'https://script.google.com/macros/s/AKfycbyHiJx7CgRquwuU3v_eGLZbtyZ2Up7jvkVPFiEGWz0DytBDHuETMp84o-rIGG_Uo1rD/exec';
-    var datos = "qr_data=" + encodeURIComponent(contenidoQR);
-
+function enviarDatosAGoogleSheets(contenidoQR, hoja) {
+    var url = 'https://script.google.com/macros/s/AKfycbzUxm2pYX0Be634Ia0ZXNjY_6BoLZv8KpIHGnCET9A2GW7IIA6dSTvRP00Vj3WK6AmwgA/exec';
+    var datos = "qr_data=" + encodeURIComponent(contenidoQR) + "&sheet=" + encodeURIComponent(hoja);
     fetch(url, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         },
         body: datos
-        
     })
     .then(function(response) {
         if (response.ok) {
             alert('Datos enviados correctamente a Google Sheets.');
         } else {
+            console.log("Hola1");
             alert('Error al enviar datos a Google Sheets.');
         }
     })
     .catch(function(error) {
+        console.log("Hola2");
         console.error('Error:', error);
         alert('Error al enviar datos a Google Sheets.');
     });
